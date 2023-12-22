c09fb77c4c36fea4d530adb1b79c6561a1e70c9e
Nikita Pavliy <nikitapavliy@gmail.com>
2023-12-22 21:06:21 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double[] GetMaxColumn(double[,] matrix)
+        {
+            double[] maxColumn = new double[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                double max = matrix[i, 0];
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+                maxColumn[i] = max;
+            }
+            return maxColumn;
+        }
+    }
+}
